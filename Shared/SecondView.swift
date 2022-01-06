2359ecb2ed7a1129f85b1276391d5756cb7f4765
Piyush Singh <marve33333@gmail.com>
2022-01-06 20:57:20 +0530

    second

---
+++ Shared/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  f
+//
+//  Created by Piyush Singh on 06/01/22.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Fuck you")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
