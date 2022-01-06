338742b1f728d0dc673fd6d6af189bc0c3def525
Piyush Singh <marve33333@gmail.com>
2022-01-06 19:04:28 +0530

    Initial Commit

---
+++ Shared/fApp.swift
@@ -0,0 +1,17 @@
+//
+//  fApp.swift
+//  Shared
+//
+//  Created by Piyush Singh on 06/01/22.
+//
+
+import SwiftUI
+
+@main
+struct fApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
